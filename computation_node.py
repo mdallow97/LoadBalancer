1b504fcfee65aa0586e784a85dfc56a3ad793915
michael dallow <mdallow@linux60805.dc.engr.scu.edu>
2019-06-02 15:52:19 -0700

    Full support for CPU information

--- computation_node.py
+++ computation_node.py
@@ -22,53 +22,28 @@ def multiplyMatrices(matrix_couple):
 
     return result
 
-'''
-def getCPUinfo():
-    \''' Return the information in /proc/cpuinfo
-    as a dictionary in the following format:
-    cpu_info['proc0']={...}
-    cpu_info['proc1']={...}
-
-    \'''
-
-    cpuinfo=OrderedDict()
-    procinfo=OrderedDict()
-
-    nprocs = 0
-    with open('/proc/cpuinfo') as f:
-        for line in f:
-            if not line.strip():
-                # end of one processor
-                cpuinfo['proc%s' % nprocs] = procinfo
-                nprocs=nprocs+1
-                # Reset
-                procinfo=OrderedDict()
-            else:
-                if len(line.split(':')) == 2:
-                    procinfo[line.split(':')[0].strip()] = line.split(':')[1].strip()
-                else:
-                    procinfo[line.split(':')[0].strip()] = ''
-
-    return cpuinfo
-
-
 def getSpecifications():
-
-    cpuinfo = getCPUinfo()
-    numCPUs = 0
-    for processor in cpuinfo.keys():
-        numCPUs += 1
-        print(cpuinfo[processor]['model name'])
-
-    print("Total number of processor: ", numCPUs)
-'''
-
-def getSpecifications():
+    #Get CPU information
+    # Assumes all CPUs on one system are the same
     cpu_model = cpuinfo.cpu.info[0]['model name']
     cpu_cores = cpuinfo.cpu.info[0]['cpu cores']
-    print("# processors?: ", len(cpuinfo.cpu.info))
+    num_cpus  = len(cpuinfo.cpu.info)
+    
+    index = 0
+    for i in range(len(cpu_model)):
+        if cpu_model[i] == '@':
+            index = i+1
+            break
 
+    clock_rate_str = ""
+    for i in range(index, len(cpu_model)):
+        if cpu_model[i] == 'G':
+            break
+        clock_rate_str += cpu_model[i]
 
+    clock_rate = float(clock_rate_str)
+    
+        
 if len(sys.argv) != 3:
     print("Format: python computation_node.py <Director_IP> <Director_Port>")
     exit()
